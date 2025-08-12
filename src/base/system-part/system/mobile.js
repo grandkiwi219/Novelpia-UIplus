8fa17a340f0640800ee5ab16744f447d29f2d57b
grandkiwi219 <grandkiwi219@users.noreply.github.com>
2025-08-12 18:30:44 +0900

    Fix, Edit

--- src/base/system-part/system/mobile.js
+++ src/base/system-part/system/mobile.js
@@ -156,53 +156,77 @@ npup.options.mobile.options['bottom-heart-alarm'].system = function(r) {
 
             ob.disconnect();
 
-            new MutationObserver((mus2, ob2) => {
-                const continue_ep = document.querySelector('.btn-view-episode');
+            setBottomHeartAlarm(continue_ep, true);
+
+            let is_changed = false;
+
+            const continueObserver = new MutationObserver((mus2, ob2) => {
+                is_changed = true;
 
                 ob2.disconnect();
 
-                setBottomHeartAlarm(continue_ep);
-            }).observe(document.querySelector('.btn-view-episode'), observer_setup);
+                setBottomHeartAlarm(document.querySelector('.btn-view-episode'), true);
+            });
+            
+            continueObserver.observe(document.querySelector('.btn-view-episode'), observer_setup);
+
+            const continueInterval = setInterval(() => {
+                if (document.querySelector('.loads').style.display != 'none') return;
+
+                continueObserver.disconnect();
+                
+                // 인터넷 속도가 느려 로딩 페이지가 오랫동안 보이고 continueObserver가 변화를 감지하기 전에 로딩 페이지를 닫는 버튼을 눌러버린다면
+                // 이어보기에 추가 변화가 없다고 감지할 수 있음.
+                if (!is_changed) npup.log('이어보기에 추가 변화가 없습니다.');
+                else npup.log('이어보기에 변화가 있었습니다.');
+
+                clearInterval(continueInterval);
+            }, 1.5 * 1000);
         }).observe(document.body, observer_setup);
     else
         return;
 
     /* 위치 속성은 css에서 / bottom-like-alarm 참고 */
+
+    /**
+     * bottom-heart-alarm system 함수
+     * @param {Element} continue_ep 측정할 이어보기 html 요소
+     * @param {boolean} comic 웹만화인가
+     */
+    const setBottomHeartAlarm = (continue_ep, comic = false) => {
+        const bottom_button = 'btn-view-episode';
+        const inner_style = 'width: 40px; height: 44px; padding: 11px 0;';
+
+        let like = document.getElementsByClassName('sbm_icon_heart')[0].parentElement.cloneNode(true);
+        let alarm = document.getElementsByClassName('sbm_icon_alert')[0].parentElement.cloneNode(true);
+
+        const ep_width = continue_ep.offsetWidth/* getBoundingClientRect().width */;
+        let style = document.createElement('style');
+        style.id = `${npup.project.prefix.css}${this.key}`
+
+        if (ep_width != 0) {    // display: none;
+            style.insertAdjacentHTML('afterbegin', epWidth(ep_width));
+            document.head.appendChild(style);
+        }
+        else {
+            if (!comic)
+                widthObserver(continue_ep, style);
+            else
+                widthComicObserver(continue_ep, style);
+        }
+
+        [like, alarm].forEach(el => {
+            el.classList.add(bottom_button);
+            el.firstElementChild.classList.add('bg-black');
+            el.firstElementChild.classList.add('mobile_show');
+            el.firstElementChild.classList.remove('s_inv'); // novelpia dark class 없애서 다크모드에서 화이트가 되는 현상 제거
+            el.firstElementChild.style = inner_style;
+
+            continue_ep.insertAdjacentElement('afterend', el);
+        });
+    }
 }
 
-/**
- * bottom-heart-alarm system 함수
- * @param {Element} continue_ep 측정할 이어보기 html 요소
- * @param {boolean} comic 웹만화인가
- */
-function setBottomHeartAlarm(continue_ep, comic = false) {
-    const bottom_button = 'btn-view-episode';
-    const inner_style = 'width: 40px; height: 44px; padding: 11px 0;';
-
-    let like = document.getElementsByClassName('sbm_icon_heart')[0].parentElement.cloneNode(true);
-    let alarm = document.getElementsByClassName('sbm_icon_alert')[0].parentElement.cloneNode(true);
-
-    const ep_width = continue_ep.offsetWidth/* getBoundingClientRect().width */;
-    let style = document.createElement('style');
-    style.id = `${npup.project.prefix.css}${this.key}`
-
-    if (ep_width != 0) style.insertAdjacentHTML('afterbegin', epWidth(ep_width)), document.head.appendChild(style);
-    else 
-        if (!comic)
-            widthObserver(continue_ep, style);
-        else 
-            widthComicObserver(continue_ep, style);
-
-    [like, alarm].forEach(el => {
-        el.classList.add(bottom_button);
-        el.firstElementChild.classList.add('bg-black');
-        el.firstElementChild.classList.add('mobile_show');
-        el.firstElementChild.classList.remove('s_inv'); // novelpia dark class 없애서 다크모드에서 화이트가 되는 현상 제거
-        el.firstElementChild.style = inner_style;
-
-        continue_ep.insertAdjacentElement('afterend', el);
-    });
-}
 
 
 /**
@@ -231,21 +255,29 @@ function widthObserver(continue_ep, style) {
     }).observe(document.body, { ...observer_setup, attributes: true });
 }
 
+let width_comic_observer = undefined;
+
 /**
  * 대형화면에서 시작해서 이어보기 크기가 display: none; 상태에서 width가 0이 되어 불편하게 보이는 것을 방지
  * @param {Element} continue_ep 측정할 이어보기 html 요소
  * @param {HTMLStyleElement} style 미리 생성해놓은 style 노드
  */
-function widthObserver(continue_ep, style) {
-    if (window.innerWidth > 891) {
-        window.addEventListener('resize', resizeListener);
+function widthComicObserver(continue_ep, style) {
+    if (width_comic_observer) {
+        window.removeEventListener('resize', width_comic_observer);
+        width_comic_observer = undefined;
+    }
 
-        function resizeListener() {
+    if (window.innerWidth > 891) {
+        width_comic_observer = function resizeListener() {
             if (window.innerWidth <= 891) {
                 setWidth(continue_ep, style);
-                window.removeEventListener('resize', resizeListener);
+                window.removeEventListener('resize', width_comic_observer);
+                width_comic_observer = undefined;
             }
         }
+
+        window.addEventListener('resize', width_comic_observer);
     }
     else
         setWidth(continue_ep, style);
