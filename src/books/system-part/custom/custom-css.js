0e826bd5b421b4ba4198e8e9af9a51c3a021d7e4
grandkiwi219 <grandkiwi219@users.noreply.github.com>
2025-08-18 20:29:36 +0900

    Add: id log

--- src/books/system-part/custom/custom-css.js
+++ src/books/system-part/custom/custom-css.js
@@ -1,5 +1,5 @@
 npup.options.custom_css.options['books-custom-css'].system = function (r) {
     tryChecker(() => {
         styleInjection(`${npup.project.prefix.css}${this.key}`, r[this.key]);
-    }, '커스텀', 'css');
+    }, '커스텀', 'css', `Id: ${npup.project.prefix.css}${this.key}`);
 }
