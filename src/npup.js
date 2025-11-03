ade3bb907b7638745abb2c015c11bf9a8d454f30
grandkiwi219 <grandkiwi219@users.noreply.github.com>
2025-11-04 00:17:08 +0900

    Edit

--- src/npup.js
+++ src/npup.js
@@ -5,7 +5,7 @@ const npup = {
 
     project: {
         name: 'NPup',
-        version: '3.10.3.0',
+        version: '3.10.3.1',
         get prefix() {
             return {
                 css: this.name.toLowerCase() + '-',
