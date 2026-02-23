0346cdcb136e878c52c43bc3d636fa00be63d7ab
grandkiwi219 <grandkiwi219@users.noreply.github.com>
2026-02-24 02:04:28 +0900

    Edit: remove

--- src/base/system-part/system/viewer.js
+++ src/base/system-part/system/viewer.js
@@ -95,18 +95,6 @@ function iconSetup() {
 
 
 
-/* viewerCa['click-alert'].system = function(r) {
-    if (!engineChecker('뷰어')) return;
-
-    npup.log('우클릭 제거 준비가 완료되었습니다.');
-
-    scriptInjection('src/base/file/delete-click-alert.js');
-
-    // nd r-d
-} */
-
-
-
 viewerCa['dbl-vote'].system = function(r) {
     // const std_novel_el = 'novel_text';
 
